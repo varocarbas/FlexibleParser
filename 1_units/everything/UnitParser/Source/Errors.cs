2d202a08b8da5d31a9db4bb0d64a94474201fdfc
varocarbas <contact@customsolvers.com>
2016-08-08 03:20:11 +0200

    First commit.

---
+++ 1_units/everything/UnitParser/Source/Errors.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FlexibleParser
+{
+    public partial class UnitP
+    {
+        ///<summary><para>Determines whether wrong inputs trigger an exception or not.</para></summary>
+        public enum ExceptionHandlingTypes
+        {
+            ///<summary><para>Wrong inputs never trigger an exception. Similar to standard .NET TryParse methods.</para></summary>
+            NeverTriggerException = 0,
+            ///<summary><para>Wrong inputs always trigger an exception. Similar to standard .NET Parse methods.</para></summary>
+            AlwaysTriggerException
+        };
+
+        ///<summary><para>All the supported error types.</para></summary>
+        public enum ErrorTypes 
+        {
+            ///<summary><para>No error occurred.</para></summary>
+            None = 0,
+            
+            ///<summary>
+            ///<para>Associated with invalid operations between UnitP variables not provoked by numeric errors.</para>
+            ///<para>Examples: addition of UnitP variables with different types; multiplication of UnitP variables outputing an unsupported unit.</para>
+            ///</summary>
+            InvalidOperation,
+            ///<summary>
+            ///<para>Associated with faulty instantiations of UnitP variables.</para>
+            ///<para>Examples: relying on an unsupported input string; including prefixes not supported in that specific context.</para>
+            ///</summary>
+            InvalidUnit,
+            ///<summary>
+            ///<para>Associated with invalid operations provoked by numeric overflow or arithmetic errors.</para>
+            ///<para>Examples: multiplication of too big values; division by zero.</para>
+            ///</summary>
+            NumericError,
+            ///<summary>
+            ///<para>Associated with errors triggered when parsing numeric inputs.</para>
+            ///<para>Example: new UnitP("unit") rather than new UnitP("number unit").</para>
+            ///</summary>
+            NumericParsingError,
+            ///<summary>
+            ///<para>Associated with unit conversion errors .</para>
+            ///<para>Example: UnitP("1 m/s").ConvertCurrentUnitTo("m/s2").</para>
+            ///</summary>
+            InvalidUnitConversion,
+        }
+
+        ///<summary><para>Stores all the information and performs all the actions related to UnitP error and exception management.</para></summary>
+        public class ErrorInfo
+        {
+            public readonly ErrorTypes Type;
+            public readonly ExceptionHandlingTypes ExceptionHandling;
+            public readonly string Message;
+
+            public ErrorInfo() { }
+
+            public ErrorInfo(ErrorInfo errorInfo) 
+            {
+                if (errorInfo == null) errorInfo = new ErrorInfo();
+                
+                Type = errorInfo.Type;
+                ExceptionHandling = errorInfo.ExceptionHandling;
+                Message = errorInfo.Message;
+            }
+
+            public ErrorInfo(ErrorTypes type, ExceptionHandlingTypes exceptionHandling = ExceptionHandlingTypes.NeverTriggerException)
+            {
+                Type = type;
+                ExceptionHandling = exceptionHandling;
+                Message = GetMessage(type);
+
+                if (ExceptionHandling == ExceptionHandlingTypes.AlwaysTriggerException)
+                {
+                    throw new Exception(Message);
+                }  
+            }
+
+            private string GetMessage(ErrorTypes type)
+            {
+                string outString = "";
+
+                if (type == ErrorTypes.InvalidOperation)
+                {
+                    outString = "Invalid Operation. Some operands are incompatible among them or refer to invalid units.";
+                }
+                else if (type == ErrorTypes.InvalidUnit)
+                {
+                    outString = "Invalid Input. The input string doesn't match any supported unit.";
+                }
+                else if (type == ErrorTypes.NumericError)
+                {
+                    outString = "Numeric Error. An invalid mathematical operation has been performed.";
+                }
+                else if (type == ErrorTypes.NumericParsingError)
+                {
+                    outString = "Numeric Parsing Error. The input doesn't match the expected number + space + unit format.";
+                }
+                else if (type == ErrorTypes.InvalidUnitConversion)
+                {
+                    outString = "Invalid Unit Conversion. The unit conversion cannot be performed with these inputs.";
+                }
+
+                return outString;
+            }
+
+            public static bool operator ==(ErrorInfo first, ErrorInfo second)
+            {
+                return NoNullEquals(first, second);
+            }
+
+            public static bool operator !=(ErrorInfo first, ErrorInfo second)
+            {
+                return !NoNullEquals(first, second);
+            }
+
+            public bool Equals(ErrorInfo other)
+            {
+                return
+                (
+                    object.Equals(other, null) ? false :
+                    ErrorsAreEqual(this, other)
+                );
+            }
+
+            public override bool Equals(object obj)
+            {
+                return Equals(obj as ErrorInfo);
+            }
+
+            public override int GetHashCode() { return 0; }
+        }
+    }
+}
---
+++ 1_units/everything/UnitParser/Source/Keywords/Private/Keywords_Private_Prefixes.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FlexibleParser
+{
+    public partial class UnitP
+    {
+        //Relates the SI prefixes (SIPrefixes enum) with their values (constants in the SIPrefixValues class).
+        internal static Dictionary<SIPrefixes, decimal> AllSIPrefixes = 
+        new Dictionary<SIPrefixes, decimal>()
+        {
+            { SIPrefixes.Yotta, SIPrefixValues.Yotta }, 
+            { SIPrefixes.Zetta, SIPrefixValues.Zetta },
+            { SIPrefixes.Exa, SIPrefixValues.Exa }, 
+            { SIPrefixes.Peta, SIPrefixValues.Peta },
+            { SIPrefixes.Tera, SIPrefixValues.Tera }, 
+            { SIPrefixes.Giga, SIPrefixValues.Giga },
+            { SIPrefixes.Mega, SIPrefixValues.Mega }, 
+            { SIPrefixes.Kilo, SIPrefixValues.Kilo },
+            { SIPrefixes.Hecto, SIPrefixValues.Hecto }, 
+            { SIPrefixes.Deca, SIPrefixValues.Deca },
+            { SIPrefixes.Deci, SIPrefixValues.Deci }, 
+            { SIPrefixes.Centi, SIPrefixValues.Centi },
+            { SIPrefixes.Milli, SIPrefixValues.Milli }, 
+            { SIPrefixes.Micro, SIPrefixValues.Micro },
+            { SIPrefixes.Nano, SIPrefixValues.Nano }, 
+            { SIPrefixes.Pico, SIPrefixValues.Pico },
+            { SIPrefixes.Femto, SIPrefixValues.Femto }, 
+            { SIPrefixes.Atto, SIPrefixValues.Atto },
+            { SIPrefixes.Zepto, SIPrefixValues.Zepto }, 
+            { SIPrefixes.Yocto, SIPrefixValues.Yocto }
+        };
+
+        //Relates the SI prefix strings (SIPrefixSymbols enum) with their values (constants in the SIPrefixValues class).
+        internal static Dictionary<string, decimal> AllSIPrefixSymbols = 
+        new Dictionary<string, decimal>()
+        {
+            { SIPrefixSymbols.Yotta, SIPrefixValues.Yotta }, 
+            { SIPrefixSymbols.Zetta, SIPrefixValues.Zetta },
+            { SIPrefixSymbols.Exa, SIPrefixValues.Exa }, 
+            { SIPrefixSymbols.Peta, SIPrefixValues.Peta },
+            { SIPrefixSymbols.Tera, SIPrefixValues.Tera }, 
+            { SIPrefixSymbols.Giga, SIPrefixValues.Giga },
+            { SIPrefixSymbols.Mega, SIPrefixValues.Mega }, 
+            { SIPrefixSymbols.Kilo, SIPrefixValues.Kilo },
+            { SIPrefixSymbols.Hecto, SIPrefixValues.Hecto }, 
+            { SIPrefixSymbols.Deca, SIPrefixValues.Deca },
+            { SIPrefixSymbols.Deci, SIPrefixValues.Deci }, 
+            { SIPrefixSymbols.Centi, SIPrefixValues.Centi },
+            { SIPrefixSymbols.Milli, SIPrefixValues.Milli }, 
+            { SIPrefixSymbols.Micro, SIPrefixValues.Micro },
+            { SIPrefixSymbols.Nano, SIPrefixValues.Nano }, 
+            { SIPrefixSymbols.Pico, SIPrefixValues.Pico },
+            { SIPrefixSymbols.Femto, SIPrefixValues.Femto }, 
+            { SIPrefixSymbols.Atto, SIPrefixValues.Atto },
+            { SIPrefixSymbols.Zepto, SIPrefixValues.Zepto }, 
+            { SIPrefixSymbols.Yocto, SIPrefixValues.Yocto }
+        };
+
+        //Relates the binary prefixes (BinaryPrefixes enum) with their values (constants in the BinaryPrefixValues class).
+        internal static Dictionary<BinaryPrefixes, decimal> AllBinaryPrefixes = 
+        new Dictionary<BinaryPrefixes, decimal>()
+        {
+            { BinaryPrefixes.Kibi, BinaryPrefixValues.Kibi }, 
+            { BinaryPrefixes.Mebi, BinaryPrefixValues.Mebi },
+            { BinaryPrefixes.Gibi, BinaryPrefixValues.Gibi }, 
+            { BinaryPrefixes.Tebi, BinaryPrefixValues.Tebi },
+            { BinaryPrefixes.Pebi, BinaryPrefixValues.Pebi }, 
+            { BinaryPrefixes.Exbi, BinaryPrefixValues.Exbi },
+            { BinaryPrefixes.Zebi, BinaryPrefixValues.Zebi }, 
+            { BinaryPrefixes.Yobi, BinaryPrefixValues.Yobi }
+        };
+
+        //Relates the binary prefix strings (BinaryPrefixSymbols enum) with their values (constants in the BinaryPrefixValues class).
+        internal static Dictionary<string, decimal> AllBinaryPrefixSymbols = 
+        new Dictionary<string, decimal>()
+        {
+            { BinaryPrefixSymbols.Kibi, BinaryPrefixValues.Kibi }, 
+            { BinaryPrefixSymbols.Mebi, BinaryPrefixValues.Mebi },
+            { BinaryPrefixSymbols.Gibi, BinaryPrefixValues.Gibi }, 
+            { BinaryPrefixSymbols.Tebi, BinaryPrefixValues.Tebi },
+            { BinaryPrefixSymbols.Pebi, BinaryPrefixValues.Pebi }, 
+            { BinaryPrefixSymbols.Exbi, BinaryPrefixValues.Exbi },
+            { BinaryPrefixSymbols.Zebi, BinaryPrefixValues.Zebi }, 
+            { BinaryPrefixSymbols.Yobi, BinaryPrefixValues.Yobi }
+        };
+
+        private static Dictionary<string, string> AllSIPrefixNames = 
+        AllSIPrefixSymbols.ToDictionary(x => x.Key, x => AllSIPrefixes.First(y => y.Value == x.Value).Key.ToString().ToLower());
+
+        private static IEnumerable<decimal> BigSIPrefixValues = 
+        AllSIPrefixes.Where(x => x.Value > 1m).Select(x => x.Value).OrderByDescending(x => x);
+
+        private static IEnumerable<decimal> SmallSIPrefixValues =
+        AllSIPrefixes.Where(x => x.Value < 1m).Select(x => x.Value).OrderBy(x => x);
+
+        private static Dictionary<string, string> AllBinaryPrefixNames =
+        AllBinaryPrefixSymbols.ToDictionary(x => x.Key, x => AllBinaryPrefixes.First(y => y.Value == x.Value).Key.ToString().ToLower());
+
+        private static IEnumerable<decimal> BigBinaryPrefixValues =
+        AllBinaryPrefixes.Where(x => x.Value > 1m).Select(x => x.Value).OrderByDescending(x => x);
+
+        private static IEnumerable<decimal> SmallBinaryPrefixValues =
+        AllBinaryPrefixes.Where(x => x.Value < 1m).Select(x => x.Value).OrderBy(x => x);
+    }
+}
