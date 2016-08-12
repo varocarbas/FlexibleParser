6ae6d6bd13ceff60db2233f4f58809c74db9155a
varocarbas <contact@customsolvers.com>
2016-08-12 12:13:33 +0200

    Unit prefixes improvements.

---
+++ all_code/Test/1_units/README.md
@@ -0,0 +1,7 @@
+# FlexibleParser
+
+##1. UnitParser
+
+I will be writing these contents during the next days.
+
+Meanwhile, you can take a look at the sample codes in the currently-being-updated-too [test application](https://github.com/varocarbas/FlexibleParser/blob/master/1_units/source/everything/Test/Program.cs). 
---
+++ all_code/Test/1_units/source/main_code/Methods/Public/Methods_Public_NonStatic.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace FlexibleParser
+{
+    public partial class UnitP
+    {
+        ///<summary><para>Converts the current unit into the target one.</para><para>Warning: different unit types will trigger an error.</para></summary>
+        ///<param name="targetUnit">Target unit.</param>
+        ///<param name="targetPrefix">Target unit prefix.</param>
+        public UnitP ConvertCurrentUnitTo(Units targetUnit, Prefix targetPrefix = null)
+        {
+            return ConvertToCommon
+            (
+                this, new UnitInfo(0m, targetUnit, new Prefix(targetPrefix))
+            );
+        }
+
+        ///<summary><para>Converts the current unit into the target one.</para><para>Warning: different unit types will trigger an error.</para></summary>
+        ///<param name="targetUnitString">Target unit string.</param>
+        public UnitP ConvertCurrentUnitTo(string targetUnitString)
+        {
+            return ConvertToCommon(this, targetUnitString);
+        }
+
+        ///<summary><para>Returns the string representations associated with the current unit.</para></summary>
+        ///<param name="otherStringsToo">When true, all the supported string representations (case doesn't matter) other than symbols (case matters) are also included.</param>  
+        public ReadOnlyCollection<string> GetStringsForCurrentUnit(bool otherStringsToo = false)
+        {
+            return GetStringsUnitCommon(Unit, otherStringsToo).AsReadOnly();
+        }
+
+        ///<summary><para>Returns the string representations associated with the current unit type.</para></summary>
+        ///<param name="otherStringsToo">When true, all the supported string representations (case doesn't matter) other than symbols (case matters) are also included.</param>        
+        public ReadOnlyCollection<string> GetStringsForCurrentType(bool otherStringsToo = false)
+        {
+            return GetStringsTypeCommon(UnitType, otherStringsToo).AsReadOnly();
+        }
+
+        ///<summary><para>Returns the string representations associated with the current unit type and system.</para></summary>
+        ///<param name="otherStringsToo">When true, all the supported string representations (case doesn't matter) other than symbols (case matters) are also included.</param>  
+        public ReadOnlyCollection<string> GetStringsForCurrentTypeAndSystem(bool otherStringsToo = false)
+        {
+            return GetStringsTypeAndSystemCommon(UnitType, UnitSystem, otherStringsToo).AsReadOnly();
+        }
+
+        ///<summary><para>Returns the members of the Units enum which are associated with the current unit type.</para></summary> 
+        public ReadOnlyCollection<Units> GetUnitsForCurrentType()
+        {
+            return GetUnitsTypeCommon(UnitType).AsReadOnly();
+        }
+
+        ///<summary><para>Returns the members of the Units enum which are associated with the current unit type and system.</para></summary>
+        public ReadOnlyCollection<Units> GetUnitsForCurrentTypeAndSystem()
+        {
+            return GetUnitsTypeAndSystemCommon(UnitType, UnitSystem).AsReadOnly();
+        }
+
+        ///<summary><para>Returns the type of the current unit.</para></summary>
+        public UnitTypes GetCurrentUnitType()
+        {
+            return GetTypeFromUnit(Unit);
+        }
+
+        ///<summary><para>Returns the system of the current unit.</para></summary>  
+        public UnitSystems GetCurrentUnitSystem()
+        {
+            return GetSystemFromUnit(Unit, false, true);
+        }
+
+        ///<summary><para>Removes all the prefixes of the current UnitP variable.</para></summary>  
+        public UnitP RemoveGlobalPrefixesFromCurrentVariable()
+        {
+            return 
+            (
+                new UnitP
+                (
+                    //Normalising means converting prefix and value into BigNumberExponent.
+                    //In the final output, the BigNumberExponent will be minimal (within the
+                    //value capabilities) because of the subsequent improvements.
+                    NormaliseUnitInfo(new UnitInfo(this)), this, true
+                )
+            );
+        }
+    }
+}
