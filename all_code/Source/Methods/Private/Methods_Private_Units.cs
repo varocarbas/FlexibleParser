d3bd2034d48f8f847dba3f13a5fa98a969c0a40f
varocarbas <contact@customsolvers.com>
2016-08-26 23:31:16 +0200

    More types and units.

---
+++ all_code/Source/Methods/Private/Methods_Private_Units.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FlexibleParser
+{
+    public partial class UnitP
+    {
+        private static UnitInfo RemoveAllUnitInformation(UnitInfo unitInfo)
+        {
+            unitInfo.Unit = Units.None;
+            unitInfo.System = UnitSystems.None;
+            unitInfo.Type = UnitTypes.None;
+
+            return unitInfo;
+        }
+
+        private static UnitInfo ImproveUnitParts(UnitInfo unitInfo)
+        {
+            //GetUnitParts isn't just meant to get parts, but also to expand/simplify them.
+            //That's why calling this method is one of the first steps when improving/analysing
+            //a compound regardless of the fact of unit parts already being present.
+            return GetUnitParts(unitInfo);
+        }
+
+        private static bool IsUnnamedUnit(Units unit)
+        {
+            return
+            (
+                //During the calculations, ImperialAndUSCS (Units.ValidImperialUSCSUnit) is used for both
+                //Imperial & USCS. Units.ValidImperialUnit/Units.ValidUSCSUnit are only considered when
+                //showing information to the user. 
+                //DefaultUnnamedUnits expects only one unnamed unit per system; Units.ValidImperialUSCSUnit
+                //is already associated with Systems.Imperial, what explains the additional check below.
+                //Logically, this implementation is fine as it is because adding multi-dimensional support to
+                //DefaultUnnamedUnits would provoke an unnecessary waste of resources.
+                unit == Units.ValidImperialUnit || 
+                DefaultUnnamedUnits.ContainsValue(unit)
+            );
+        }
+
+        private static UnitInfo RemoveUnitPart(UnitInfo unitInfo, UnitPart part)
+        {
+            unitInfo.InitialPositions.Remove(part);
+            unitInfo.Parts.Remove(part);
+
+            return unitInfo;
+        }
+
+        //This method assumes a normalised UnitInfo variable (i.e., without prefixes).
+        private static UnitInfo GetBestPrefixForTarget(UnitInfo unitInfo, int targetExponent, PrefixTypes prefixType, bool modifyBaseTenExponent = false)
+        {
+            if (targetExponent == 0 || prefixType == PrefixTypes.None) return unitInfo;
+
+            if (targetExponent > 24) targetExponent = 24;
+            else if(targetExponent < -24) targetExponent = -24;
+
+            UnitInfo targetInfo = RaiseToIntegerExponent(10, targetExponent);
+
+            foreach (decimal prefixFactor in GetAllPrefixFactors(targetExponent, prefixType))
+            {
+                if ((targetExponent > 0 && targetInfo.Value >= prefixFactor) || (targetExponent < 0 && targetInfo.Value <= prefixFactor))
+                {
+                    unitInfo = IncludeRemainingTargetPrefix
+                    (
+                        unitInfo, targetInfo, new UnitInfo(prefixFactor)
+                    );
+                    unitInfo.Prefix = new Prefix(prefixFactor, unitInfo.Prefix.PrefixUsage);
+                    break;
+                }
+            }
+
+            if (modifyBaseTenExponent)
+            {
+                unitInfo.BaseTenExponent -= targetExponent;
+            }
+
+            return unitInfo;
+        }
+
+        private static UnitInfo IncludeRemainingTargetPrefix(UnitInfo unitInfo, UnitInfo expectedTarget, UnitInfo actualTarget)
+        {
+            UnitInfo remInfo = NormaliseUnitInfo
+            (
+                PerformManagedOperationValues
+                (
+                    expectedTarget, actualTarget, Operations.Division
+                )
+            );
+
+            return PerformManagedOperationValues(unitInfo, remInfo, Operations.Multiplication);
+        }
+
+        private static IEnumerable<decimal> GetAllPrefixFactors(int targetExponent, PrefixTypes prefixType)
+        {
+            IEnumerable<decimal> allPrefixes = null;
+
+            if (prefixType == PrefixTypes.SI)
+            {
+                allPrefixes =
+                (
+                    targetExponent > 0 ? BigSIPrefixValues :
+                    SmallSIPrefixValues
+                );
+            }
+            else
+            {
+                allPrefixes =
+                (
+                    targetExponent > 0 ? BigBinaryPrefixValues :
+                    SmallBinaryPrefixValues
+                );
+            }
+
+            return allPrefixes;
+        }
+
+        private static Units GetDefaultEnglishMetricUnit(UnitTypes type, UnitSystems system)
+        {
+            foreach (var item in AllUnitConversionFactors.Where(x => GetTypeFromUnit(x.Key) == type))
+            {
+                UnitSystems system2 = GetSystemFromUnit(item.Key);
+                if (system2 == UnitSystems.None) continue;
+
+                if (AllMetricEnglish[system] == AllMetricEnglish[system2])
+                {
+                    return item.Key;
+                }
+            }
+
+            return Units.None;
+        }
+
+        private static UnitInfo GetPartsFromUnit(UnitInfo unitInfo)
+        {
+            if (unitInfo.Unit == Units.None || unitInfo.Unit == Units.Unitless || IsUnnamedUnit(unitInfo.Unit))
+            {
+                return unitInfo;
+            }
+            
+            unitInfo = GetPartsFromUnitCompound(unitInfo);
+           
+            if (unitInfo.Parts.Count < 1)
+            {
+                unitInfo.Parts = new List<UnitPart>()
+                {
+                    new UnitPart(unitInfo.Unit, 1)
+                };
+            }
+
+            return unitInfo;
+        }
+
+        private static string GetUnitString(UnitInfo unitInfo)
+        {
+            string outUnit = GetUnitStringIndividual(unitInfo);
+
+            return
+            (
+                outUnit == "None" ?
+                GetUnitStringCompound(unitInfo) : outUnit
+            );
+        }
+
+        private static string GetUnitStringIndividual(UnitInfo unitInfo)
+        {
+            string unitString = "None";
+
+            if (unitInfo.Unit != Units.None && unitInfo.Unit != Units.Unitless && !IsUnnamedUnit(unitInfo.Unit))
+            {
+                if (AllUnitSymbols.ContainsValue(unitInfo.Unit))
+                {
+                    unitString = AllUnitSymbols.First(x => x.Value == unitInfo.Unit).Key;
+                    if (unitInfo.Prefix.Symbol != "")
+                    {
+                        unitString = unitInfo.Prefix.Symbol + unitString;
+                    }
+                }
+            }
+
+            return unitString;
+        }
+
+        private static Units GetUnitFromString(string input)
+        {
+            Units unit = GetUnitFromUnitSymbols(input);
+
+            return
+            (
+                unit != Units.None ? unit :
+                GetUnitFromUnitStrings(input)
+            );
+        }
+
+        private static Units GetUnitFromUnitSymbols(string input)
+        {
+            return
+            (
+                AllUnitSymbols.ContainsKey(input) ?
+                AllUnitSymbols[input] :
+                GetUnitFromUnitSymbols2(input)
+            );
+        }
+
+        private static Units GetUnitFromUnitSymbols2(string input)
+        {
+            return
+            (
+                AllUnitSymbols2.ContainsKey(input) ?
+                AllUnitSymbols2[input] : Units.None
+            );
+        }
+  
+        private static Units GetUnitFromUnitStrings(string input)
+        {
+            string inputLower = input.ToLower();
+
+            return AllUnitStrings.FirstOrDefault
+            (
+                x => (x.Key.ToLower() == inputLower || GetUnitStringPlural(x.Key.ToLower()) == inputLower)
+            )
+            .Value;
+        }
+
+        private static string GetUnitStringPlural(string unitString)
+        {
+            unitString = unitString.Replace("inches", "inch");
+            unitString = unitString.Replace("inch", "inches");
+            unitString = unitString.Replace("foot", "feet");
+
+            return
+            (
+                unitString.EndsWith("inches") || unitString.EndsWith("feet") ?
+                unitString : unitString + "s"
+            );
+        }
+
+        private static UnitTypes GetTypeFromUnitPart(UnitPart unitPart, bool ignoreExponents = false)
+        {
+            UnitPart unitPart2 = new UnitPart(unitPart);
+
+            //When comparing unit part types, the exponent is often irrelevant. For example: in the compound
+            //kg*m4, looking for m4 would yield no match (unlikely looking just for m).
+            if (ignoreExponents) unitPart2.Exponent = 1;
+
+            //Negative exponents do not affect type determination. For example, a unit consisting
+            //in just the part m-1 is wavenumber (negative exponent being relevant), but is expected
+            //to be treated as length (-1 doesn't matter) because of being used in internal calculations.
+            unitPart2.Exponent = Math.Abs(unitPart2.Exponent);
+            UnitInfo unitInfo = new UnitInfo(unitPart2.Unit, unitPart2.Prefix.Factor);
+            unitInfo.Parts = new List<UnitPart>() { unitPart2 };
+
+            UnitTypes outType = GetTypeFromUnitInfo(unitInfo);
+            return 
+            (
+                outType == UnitTypes.None && unitPart2.Exponent != 1 ?
+                //To account for cases like kg4 within compounds, expected to be understood as kg.
+                GetTypeFromUnitPart(new UnitPart(unitPart2) { Exponent = 1 }) :
+                outType
+            );
+        }
+
+        private static UnitTypes GetTypeFromUnitInfo(UnitInfo unitInfo)
+        {
+            UnitTypes outType = UnitTypes.None;
+            if (unitInfo.Parts.Count > 0)
+            {
+                outType = GetBasicCompoundType(unitInfo).Type;
+            }
+
+            return
+            (
+                outType != UnitTypes.None ?
+                outType : GetTypeFromUnit(unitInfo.Unit)
+            );
+        }
+
+        private static UnitTypes GetTypeFromUnit(Units unit)
+        {
+            return
+            (
+                unit == Units.None || unit == Units.Unitless || IsUnnamedUnit(unit) ?
+                UnitTypes.None : AllUnitTypes[unit]
+            );
+        }
+
+        private static UnitSystems GetSystemFromUnit(Units unit, bool getBasicVersion = false, bool getImperialAndUSCS = false)
+        {
+            if (unit == Units.None || unit == Units.Unitless)
+            {
+                return UnitSystems.None;
+            }
+
+            UnitSystems system = AllUnitSystems[unit];
+
+            if (getImperialAndUSCS)
+            {
+                if (system == UnitSystems.Imperial && AllImperialAndUSCSUnits.Contains(unit))
+                {
+                    system = UnitSystems.ImperialAndUSCS;
+                }
+            }
+
+            return 
+            (
+                getBasicVersion ? AllBasicSystems[system] : system
+            );
+        }
+
+        //Confirms whether the given minus sign (-) is correct. Any-5 is right, but 5-5 is wrong.
+        private static bool MinusIsOK(char[] inputArray, int i)
+        {
+            if (i > 0 && i < inputArray.Length)
+            {
+                if (char.IsNumber(inputArray[i + 1]) && !char.IsNumber(inputArray[i - 1]))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ all_code/Source/Parse/Compounds/Parse_Private_Compounds_Methods.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FlexibleParser
+{
+    public partial class UnitP
+    {
+        //Returns the string representation of the unit associated with the given compound.
+        //NOTE: this function assumes that a valid compound is already in place.
+        private static string GetUnitStringCompound(UnitInfo unitInfo)
+        {
+            if (unitInfo.Unit == Units.None) return "None";
+            else if (unitInfo.Unit == Units.Unitless) return "Unitless";
+
+            string outUnitString = "";
+            bool inNumerator = true;
+
+            foreach (UnitPart unitPart in unitInfo.Parts)
+            {
+                if (inNumerator && unitPart.Exponent < 0)
+                {
+                    inNumerator = false;
+                    if (outUnitString == "") outUnitString = "1";
+                    outUnitString = outUnitString + "/";
+                }
+                else if (outUnitString != "") outUnitString = outUnitString + "*";
+
+                string unitString = unitPart.Prefix.Symbol + AllUnitSymbols.First(x => x.Value == unitPart.Unit).Key;
+                int exponent = Math.Abs(unitPart.Exponent);
+                if (exponent != 1) unitString = unitString + exponent.ToString();
+
+                outUnitString = outUnitString + unitString;
+            }
+
+            return outUnitString;
+        }
+
+        private static UnitInfo GetBasicCompoundType(UnitInfo unitInfo)
+        {
+            foreach (var allCompound in AllCompounds)
+            {
+                foreach (var compound in allCompound.Value)
+                {
+                    if (UnitPartsMatchCompoundParts(unitInfo.Parts, compound.Parts))
+                    {
+                        unitInfo.Type = allCompound.Key;
+                        break;
+                    }
+                }
+            }
+
+            return unitInfo;
+        }
+
+        private static bool UnitPartsMatchCompoundParts(List<UnitPart> unitParts, List<CompoundPart> compoundParts)
+        {
+            if (unitParts.Count != compoundParts.Count) return false;
+
+            foreach (UnitPart part in unitParts)
+            {
+                UnitTypes type = GetTypeFromUnit(part.Unit);
+                int exponent = part.Exponent;
+                if (compoundParts.FirstOrDefault(x => x.Type == type && x.Exponent == exponent) == null)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static ParsedExponent GetCompoundExponent(string input)
+        {
+            input = input.Trim();
+            char[] inputArray = input.ToArray();
+            int i2 = 0;
+            
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                if (!char.IsNumber(inputArray[i]) && inputArray[i] != '-')
+                {
+                    i2 = i;
+                    break;
+                }
+            }
+
+            ParsedExponent outVar = new ParsedExponent(input);
+            if (i2 >= 0 && i2 <= input.Length - 1)
+            {
+                int exponent = 1;
+                if (int.TryParse(input.Substring(i2 + 1), out exponent))
+                {
+                    outVar = new ParsedExponent
+                    (
+                        input.Substring(0, i2 + 1), 
+                        exponent
+                    );
+                }
+            }
+
+            return outVar;
+        }
+
+        private static ParsedUnit UpdateUnitParts(ParsedUnit parsedUnit, StringBuilder inputSB, bool isNumerator, char symbol)
+        {
+            string input = inputSB.ToString();
+
+            ParsedExponent exponent = GetCompoundExponent(input);
+            if (!isNumerator) exponent.Exponent = -1 * exponent.Exponent;
+
+            ParsedUnit parsedUnit2 = StartIndividualUnitParse
+            (
+                new ParsedUnit
+                (
+                    0m, exponent.AfterString,
+                    parsedUnit.UnitInfo.Prefix.PrefixUsage
+                )
+            );
+
+            if (parsedUnit2.UnitInfo.Unit == Units.None)
+            {
+                parsedUnit.UnitInfo.Error = new ErrorInfo(ErrorTypes.InvalidUnit);
+                return parsedUnit; 
+            }
+
+            parsedUnit = AddInformationToValidUnitPart
+            (
+                parsedUnit, symbol, exponent, input
+            );
+
+            parsedUnit.UnitInfo.Parts.Add
+            (
+                new UnitPart
+                (
+                    parsedUnit2.UnitInfo.Unit, parsedUnit2.UnitInfo.Prefix.Factor,
+                    exponent.Exponent
+                )
+            );
+
+            return parsedUnit;
+        }
+
+        private static ParsedUnit AddInformationToValidUnitPart(ParsedUnit parsedUnit, char symbol, ParsedExponent exponent, string input)
+        {
+            if (symbol != ' ') parsedUnit.ValidCompound.Append(symbol);
+
+            if (exponent.AfterString.Trim().Length > 0)
+            {
+                parsedUnit.ValidCompound.Append(exponent.AfterString);
+            }
+
+            string exponent2 = input.Replace(exponent.AfterString, "").Trim();
+            if (exponent2.Length > 0)
+            {
+                parsedUnit.ValidCompound.Append(exponent2);
+            }
+
+            return parsedUnit;
+        }
+
+        private static bool StringCanBeCompound(string inputToParse)
+        {
+            return
+            (
+                inputToParse.Length >= 2 &&
+                inputToParse.FirstOrDefault(x => IsCompoundDescriptive(x)) != '\0'
+            );
+        }
+
+        private static bool IsCompoundDescriptive(char bit, bool ignoreNumbers = false)
+        {
+            return
+            (
+                (!ignoreNumbers && char.IsNumber(bit)) || bit == '-' ||
+                OperationSymbols[Operations.Multiplication].Contains(bit) ||
+                OperationSymbols[Operations.Division].Contains(bit)
+            );
+        }
+    }
+}
