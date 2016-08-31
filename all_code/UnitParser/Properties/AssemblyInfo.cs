a60fadf214f8d983f94f6117c70f78d77e79421c
varocarbas <contact@customsolvers.com>
2016-08-31 12:10:59 +0200

    AssemblyInfo update

--- all_code/UnitParser/Properties/AssemblyInfo.cs
+++ all_code/UnitParser/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("UnitParser")]
-[assembly: AssemblyDescription("Unit-parsing library. First part of FlexibleParser.")]
+[assembly: AssemblyDescription("Unit parsing library. Part of FlexibleParser.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Custom Solvers 2.0")]
 [assembly: AssemblyProduct("FlexibleParser")]
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
