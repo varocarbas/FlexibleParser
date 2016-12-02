9877ba70ea383767f0aa42af4519fb0ecc87ba82
varocarbas <contact@customsolvers.com>
2016-12-02 13:51:35 +0100

    NumberParser_1.0.6174.19926

---
+++ code/Test/Program.cs
@@ -0,0 +1,14 @@
+﻿﻿using System;
+using System.Collections.Generic;
+
+namespace Test
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            //NumberParser tests.
+            NumberParser.StartTest();
+        }
+    }
+}
