5037e56a3e8e9d7d429ebb689e80292b69a370cd
varocarbas <contact@customsolvers.com>
2017-05-10 13:11:26 +0200

    DateParser_1.0.6337.38019 branch creation.

---
+++ code/Test/Program.cs
@@ -0,0 +1,14 @@
+﻿﻿using System;
+using System.Collections.Generic;
+
+namespace Test
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            //DateParser tests.
+            DateParser.StartTest();
+        }
+    }
+}
