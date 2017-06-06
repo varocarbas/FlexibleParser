a634fe4b981ff0ba74eb6bcecbc420dc2c2d0d1e
varocarbas <contact@customsolvers.com>
2017-06-06 18:08:09 +0200

    New 1.0.7-based version numbering for the DLL files.

--- all_code/DateParser/Properties/AssemblyInfo.cs
+++ all_code/DateParser/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DateParser")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Date parsing library. Part of FlexibleParser.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Custom Solvers 2.0")]
 [assembly: AssemblyProduct("DateParser")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyTrademark("varocarbas")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.7")]
