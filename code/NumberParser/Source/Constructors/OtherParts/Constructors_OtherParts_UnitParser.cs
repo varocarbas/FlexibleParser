775dfced7def0b14fa6b0fe13a6e0cd4a9bb380e
varocarbas <contact@customsolvers.com>
2017-09-08 12:13:33 +0200

    New branch NumberParser_1.0.8.5.

---
+++ code/NumberParser/Source/Constructors/OtherParts/Constructors_OtherParts_UnitParser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FlexibleParser
+{
+    public partial class Number
+    {
+        ///<summary><para>Initialises a new Number instance.</para></summary>
+        ///<param name="unitP">UnitParser's UnitP variable to be used.</param>
+        public Number(dynamic unitP)
+        {
+            Number number = OtherParts.GetNumberFromUnitP(unitP);
+
+            BaseTenExponent = number.BaseTenExponent;
+            Value = number.Value;
+            Error = number.Error;
+        }
+    }
+
+    public partial class NumberO
+    {
+        ///<summary><para>Initialises a new NumberO instance.</para></summary>
+        ///<param name="unitP">UnitParser's UnitP variable to be used.</param>
+        public NumberO(dynamic unitP)
+        {
+            Number number = OtherParts.GetNumberFromUnitP(unitP);
+
+            _OtherTypes = GetAssociatedTypes(OtherTypes.AllTypes);
+            BaseTenExponent = number.BaseTenExponent;
+            Value = number.Value;
+            Error = number.Error;
+        }
+    }
+
+    public partial class NumberP
+    {
+        ///<summary><para>Initialises a new NumberP instance.</para></summary>
+        ///<param name="unitP">UnitParser's UnitP variable to be used.</param>
+        public NumberP(dynamic unitP)
+        {
+            Number number = OtherParts.GetNumberFromUnitP(unitP);
+
+            BaseTenExponent = number.BaseTenExponent;
+            Value = number.Value;
+            Error = number.Error;
+        }
+    }
+}
